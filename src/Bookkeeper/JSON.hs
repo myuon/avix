03100ae56bc3a27c4d8d9d98da0d6f6d053e9fad
myuon <ioi.joi.koi.loi@gmail.com>
2017-08-24 02:41:22 +0900

    exedit record

---
+++ src/Bookkeeper/JSON.hs
@@ -0,0 +1,39 @@
+-- from: https://gist.github.com/lotz84/5a5fd1aba842bbf111a7f5c548f40cfa
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+module Bookkeeper.JSON where
+
+import Bookkeeper.Internal (Book'(..))
+import Data.Aeson (ToJSON(..), object, (.=))
+import Data.Aeson.Types (Pair)
+import qualified Data.Text as Text
+import Data.Type.Map (Map(..), Mapping(..))
+import GHC.TypeLits (KnownSymbol, Symbol)
+import Data.Kind (Type)
+
+instance ToPairs m => ToJSON (Book' m) where
+  toJSON (Book m) = object $ toPairs m
+
+class ToPairs (m :: [Mapping Symbol Type]) where
+  toPairs :: Map m -> [Pair]
+
+instance ToPairs '[] where
+  toPairs _ = []
+
+instance (KnownSymbol k, ToJSON v, ToPairs m) => ToPairs ((k :-> v) ': m) where
+  toPairs (Ext k v m) = (Text.pack (show k) .= toJSON v) : toPairs m
+
+class ToStrPairs (m :: [Mapping Symbol Type]) where
+  toStrPairs :: Map m -> [(String, String)]
+
+instance ToStrPairs '[] where
+  toStrPairs _ = []
+
+instance (KnownSymbol k, Show v, ToStrPairs m) => ToStrPairs ((k :-> v) : m) where
+  toStrPairs (Ext k v m) = (show k, show v) : toStrPairs m
+
+bookPairs :: ToStrPairs m => Book' m -> [(String, String)]
+bookPairs (Book m) = reverse $ toStrPairs m
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,56 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 module Main where
+
+import Bookkeeper
+import Bookkeeper.Lens
+import Bookkeeper.JSON
+import Data.Aeson
+import Control.Lens
+import Linear.V2
+import GHC.OverloadedLabels
+
+type Exedit = Book
+  [ "width" :=> Int
+  , "height" :=> Int
+  , "rate" :=> Int
+  , "scale" :=> Double
+  , "length" :=> Int
+  , "audio_rate" :=> Int
+  , "audio_ch" :=> Int
+  ]
+
+defExedit :: Exedit
+defExedit = emptyBook
+  & #width =: 1280
+  & #height =: 720
+  & #rate =: 30
+  & #scale =: 1
+  & #length =: 1
+  & #audio_rate =: 44100
+  & #audio_ch =: 2
+
+_size :: Lens' Exedit (V2 Int)
+_size = lens (\r -> V2 (r ^. #width) (r ^. #height)) (\r v -> r & #width .~ v ^. _x & #height .~ v ^. _y)
+
+data Resolution = VGA | HD | FullHD | Niconico | Custom (V2 Int)
+
+_resolution :: Getter Resolution (V2 Int)
+_resolution = to $ \case
+  VGA -> V2 640 480
+  HD -> V2 1280 720
+  FullHD -> V2 1920 1080
+  Niconico -> V2 960 540
+  Custom v -> v
+
+printExo :: Exedit -> IO ()
+printExo ex = do
+  putStrLn "[exedit]"
+  mapM_ (\(k,v) -> putStrLn $ k ++ "=" ++ v) $ bookPairs ex
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  printExo defExedit
+
