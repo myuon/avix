a7e6c1306efdcd91ff0d1fad2757ec668999a491
myuon <ioi.joi.koi.loi@gmail.com>
2017-08-24 21:28:20 +0900

    extensible records

--- src/Main.hs
+++ src/Main.hs
@@ -1,53 +1,54 @@
-{-# LANGUAGE UndecidableInstances #-}
 module Main where
 
-import Bookkeeper
-import Bookkeeper.Internal (Book')
-import Bookkeeper.Lens
-import Bookkeeper.JSON
-import Data.Aeson
+import Data.Extensible
+import Data.Extensible.HList
 import Data.Proxy
+import Data.Type.Equality
 import Control.Lens
 import Linear.V2
 import GHC.TypeLits
-import Internal
+
+toPairs :: Forall (KeyValue KnownSymbol ((~) String)) xs => Record xs -> [(String, String)]
+toPairs r = henumerateFor (Proxy @(KeyValue KnownSymbol ((~) String))) r (\mem -> (:) (symbolVal (proxyAssocKey mem) , r ^. itemAt mem)) []
 
 class ExoFormat t where
   eformat :: t -> String
   def :: t
 
-type BExedit =
-  [ "width" :=> Int
-  , "height" :=> Int
-  , "rate" :=> Int
-  , "scale" :=> Double
-  , "length" :=> Int
-  , "audio_rate" :=> Int
-  , "audio_ch" :=> Int
+type ExeditR =
+  [ "width" >: Int
+  , "height" >: Int
+  , "rate" >: Int
+  , "scale" >: Double
+  , "length" >: Int
+  , "audio_rate" >: Int
+  , "audio_ch" >: Int
   ]
 
-newtype Exedit = Exedit { getExedit :: Book BExedit }
+newtype Exedit = Exedit { getExedit :: Record ExeditR }
 makeWrapped ''Exedit
 
 instance ExoFormat Exedit where
-  eformat (Exedit book)
-    = unlines $ fmap (\(k,v) -> k ++ "=" ++ v) $ bookPairs $ emptyBook
-    & #width =: (book ^. #width) ^. to show
-    & #height =: (book ^. #height) ^. to show
-    & #rate =: (book ^. #rate) ^. to show
-    & #scale =: (book ^. #scale) ^. to show
-    & #length =: (book ^. #length) ^. to show
-    & #audio_rate =: (book ^. #audio_rate) ^. to show
-    & #audio_ch =: (book ^. #audio_ch) ^. to show
+  eformat (Exedit r)
+    = unlines $ fmap (\(k,v) -> k ++ "=" ++ v) $ toPairs
+    $ #width @= show (r ^. #width)
+    <: #height @= show (r ^. #height)
+    <: #rate @= show (r ^. #rate)
+    <: #scale @= show (r ^. #scale)
+    <: #length @= show (r ^. #length)
+    <: #audio_rate @= show (r ^. #audio_rate)
+    <: #audio_ch @= show (r ^. #audio_ch)
+    <: emptyRecord
 
-  def = Exedit $ emptyBook
-    & #width =: 1280
-    & #height =: 720
-    & #rate =: 30
-    & #scale =: 1
-    & #length =: 1
-    & #audio_rate =: 44100
-    & #audio_ch =: 2
+  def = Exedit $
+    #width @= 1280
+    <: #height @= 720
+    <: #rate @= 30
+    <: #scale @= 1
+    <: #length @= 1
+    <: #audio_rate @= 44100
+    <: #audio_ch @= 2
+    <: emptyRecord
 
 _size :: Lens' Exedit (V2 Int)
 _size = lens
@@ -64,43 +65,43 @@ _resolution = to $ \case
   Niconico -> V2 960 540
   Custom v -> v
 
-type BTLObject =
-  [ "start" :=> Int
-  , "end" :=> Int
-  , "layer" :=> Int
-  , "overlay" :=> Int
-  , "camera" :=> Bool
-  , "clipping" :=> Maybe Int
-  , "parameter" :=> Parameter
-  , "figure" :=> Figure
+type TLObjectR =
+  [ "start" >: Int
+  , "end" >: Int
+  , "layer" >: Int
+  , "overlay" >: Int
+  , "camera" >: Bool
+  , "clipping" >: Maybe Int
+  , "parameter" >: Parameter
+  , "figure" >: Figure
   ]
 
 data RenderType = Standard
 
-type Parameter = Book
-  [ "_name" :=> RenderType
-  , "X" :=> Double
-  , "Y" :=> Double
-  , "Z" :=> Double
-  , "拡大率" :=> Double
-  , "透明度" :=> Double
-  , "回転" :=> Double
-  , "blend" :=> Int
+type Parameter = Record
+  [ "_name" >: RenderType
+  , "X" >: Double
+  , "Y" >: Double
+  , "Z" >: Double
+  , "拡大率" >: Double
+  , "透明度" >: Double
+  , "回転" >: Double
+  , "blend" >: Int
   ]
 
 data Color = Color Int Int Int
 
-type Figure = Book
-  [ "_name" :=> String
-  , "サイズ" :=> Int
-  , "縦横比" :=> Double
-  , "ライン幅" :=> Int
-  , "type" :=> Int
-  , "color" :=> Color
-  , "name" :=> String
+type Figure = Record
+  [ "_name" >: String
+  , "サイズ" >: Int
+  , "縦横比" >: Double
+  , "ライン幅" >: Int
+  , "type" >: Int
+  , "color" >: Color
+  , "name" >: String
   ]
 
-newtype TLObject = TLObject { getTLObject :: Book BTLObject }
+newtype TLObject = TLObject { getTLObject :: Record TLObjectR }
 makeWrapped ''TLObject
 
 printExo :: Exedit -> IO ()
