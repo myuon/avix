3d18abb94f884bcd0629e7151c06f9434d633f7c
myuon <ioi.joi.koi.loi@gmail.com>
2017-08-25 02:33:00 +0900

    reorder

--- src/Main.hs
+++ src/Main.hs
@@ -79,6 +79,47 @@ _resolution = to $ \case
   Niconico -> V2 960 540
   Custom v -> v
 
+data RGB = RGB Word8 Word8 Word8
+
+_RGB :: Getter RGB T.Text
+_RGB = to $ \case
+  RGB r g b -> T.pack $ concat [r ^. re hex, g ^. re hex, b ^. re hex]
+
+type FigureR =
+  [ "__name" >: T.Text
+  , "_サイズ" >: Int
+  , "_縦横比" >: Double
+  , "_ライン幅" >: Double
+  , "_type" >: Int
+  , "color" >: RGB
+  , "name" >: T.Text
+  ]
+
+newtype Figure = Figure { getFigure :: Record FigureR }
+makeWrapped ''Figure
+
+instance ExoFormat Figure where
+  eformat n (Figure r)
+    = T.append (format "[{}.0]\n" [n]) $ unlinePairs $ toPairs
+    $ #__name @= r ^. #__name
+    <: #_サイズ @= (r ^. #_サイズ ^. to showt)
+    <: #_縦横比 @= (r ^. #_縦横比 ^. to showt)
+    <: #_ライン幅 @= (r ^. #_ライン幅 ^. to showt)
+    <: #_type @= (r ^. #_type ^. to showt)
+    <: #color @= (r ^. #color ^. _RGB)
+    <: #name @= r ^. #name
+    <: emptyRecord
+
+  def = Figure
+    $ #__name @= "図形"
+    <: #_サイズ @= 100
+    <: #_縦横比 @= 0
+    <: #_ライン幅 @= 4000
+    <: #_type @= 1
+    <: #color @= RGB 255 255 255
+    <: #name @= ""
+    <: emptyRecord
+
 data RenderType = Standard
 
 data BlendMode
@@ -131,7 +172,7 @@ makeWrapped ''Parameter
 
 instance ExoFormat Parameter where
   eformat n (Parameter r)
-    = T.append (format "[{}.1]" [n]) $ unlinePairs $ toPairs
+    = T.append (format "[{}.1]\n" [n]) $ unlinePairs $ toPairs
     $ #__name @= "標準描画"
     <: #_X @= (r ^. #_X ^. to showt)
     <: #_Y @= (r ^. #_Y ^. to showt)
@@ -151,47 +192,6 @@ instance ExoFormat Parameter where
     <: #_透明度 @= 0
     <: #_回転 @= 0
     <: #blend @= Normal
-    <: emptyRecord
-
-data RGB = RGB Word8 Word8 Word8
-
-_RGB :: Getter RGB T.Text
-_RGB = to $ \case
-  RGB r g b -> T.pack $ concat [r ^. re hex, g ^. re hex, b ^. re hex]
-
-type FigureR =
-  [ "__name" >: T.Text
-  , "_サイズ" >: Int
-  , "_縦横比" >: Double
-  , "_ライン幅" >: Double
-  , "_type" >: Int
-  , "color" >: RGB
-  , "name" >: T.Text
-  ]
-
-newtype Figure = Figure { getFigure :: Record FigureR }
-makeWrapped ''Figure
-
-instance ExoFormat Figure where
-  eformat n (Figure r)
-    = T.append (format "[{}.0]\n" [n]) $ unlinePairs $ toPairs
-    $ #__name @= r ^. #__name
-    <: #_サイズ @= (r ^. #_サイズ ^. to showt)
-    <: #_縦横比 @= (r ^. #_縦横比 ^. to showt)
-    <: #_ライン幅 @= (r ^. #_ライン幅 ^. to showt)
-    <: #_type @= (r ^. #_type ^. to showt)
-    <: #color @= (r ^. #color ^. _RGB)
-    <: #name @= r ^. #name
-    <: emptyRecord
-
-  def = Figure
-    $ #__name @= "図形"
-    <: #_サイズ @= 100
-    <: #_縦横比 @= 0
-    <: #_ライン幅 @= 4000
-    <: #_type @= 1
-    <: #color @= RGB 255 255 255
-    <: #name @= ""
     <: emptyRecord
 
 type TLObjectR =
